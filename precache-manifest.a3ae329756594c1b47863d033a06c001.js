5e2df87032950842d042aa999feda009ea02f23b
unknown <irajhussain@gmail.com>
2020-08-06 17:58:34 +0200

    Updates

---
+++ precache-manifest.a3ae329756594c1b47863d033a06c001.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4937b58837b81a48fa492b92a5e37232",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "8deb6de161e162c2083e",
+    "url": "/face-recognition-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d92ea62ed03e14b0c1c7",
+    "url": "/face-recognition-app/static/css/main.f596e4fb.chunk.css"
+  },
+  {
+    "revision": "8deb6de161e162c2083e",
+    "url": "/face-recognition-app/static/js/2.d5d45a8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-app/static/js/2.d5d45a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92ea62ed03e14b0c1c7",
+    "url": "/face-recognition-app/static/js/main.7b561b07.chunk.js"
+  },
+  {
+    "revision": "bceb935893d6fda73c2d",
+    "url": "/face-recognition-app/static/js/runtime-main.b9a7e2cf.js"
+  },
+  {
+    "revision": "727f23f2944947bd2cb9461fdf2357d7",
+    "url": "/face-recognition-app/static/media/brain.727f23f2.png"
+  }
+]);
